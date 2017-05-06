c20cb0456a81a9715f3420e073ac53f7a1b27db6
FASTTRACK\nkumar <nkumar@fasttrack.com.au>
2017-05-07 08:08:37 +1000

    Ui for search result and order screen

---
+++ src/app/list-results/list-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-results',
+  templateUrl: './list-results.component.html',
+  styleUrls: ['./list-results.component.css']
+})
+export class ListResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
