22e40f699cff0f5b73781bc6e8dd99b62ac73d2d
Prasanna Bhat <pbhat@fasttrack.com.au>
2017-05-06 15:00:41 +1000

    dist folder added

---
+++ dist/main.bundle.js
@@ -0,0 +1,266 @@
+webpackJsonp([1,4],{
+
+/***/ 134:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(26)();
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 135:
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(26)();
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ 137:
+/***/ (function(module, exports) {
+
+module.exports = "<app-capture-photo></app-capture-photo>"
+
+/***/ }),
+
+/***/ 138:
+/***/ (function(module, exports) {
+
+module.exports = "<video id=\"video\" width=\"640\" height=\"480\" autoplay (click)=\"onScan()\" [hidden] = \"hideVedio\" ></video>\n<button id=\"snap\" (click)=\"onScan()\">Snap Photo</button>\n<button id=\"snap\" (click)=\"startVideoCamera()\">Start Photo</button>\n<i class=\"fa fa-camera\" aria-hidden=\"true\"></i>\n<canvas id=\"canvas\" width=\"640\" height=\"480\" [hidden]= \"hidePhoto\"></canvas>\n"
+
+/***/ }),
+
+/***/ 163:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(70);
+
+
+/***/ }),
+
+/***/ 69:
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = 69;
+
+
+/***/ }),
+
+/***/ 70:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__(76);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__(78);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__(80);
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 77:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(5);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app works!';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_3" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__(137),
+        styles: [__webpack_require__(134)]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ 78:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__(20);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(5);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__(74);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__(75);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__app_component__ = __webpack_require__(77);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__capture_photo_capture_photo_component__ = __webpack_require__(79);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_5__capture_photo_capture_photo_component__["a" /* CapturePhotoComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_4__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ 79:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(5);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return CapturePhotoComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var CapturePhotoComponent = (function () {
+    function CapturePhotoComponent() {
+        this.hideVedio = true;
+        this.hidePhoto = true;
+    }
+    CapturePhotoComponent.prototype.ngOnInit = function () { };
+    CapturePhotoComponent.prototype.startVideoCamera = function () {
+        this.hideVedio = false;
+        this.hidePhoto = true;
+        // Grab elements, create settings, etc.
+        var video = document.getElementById('video');
+        // Get access to the camera!
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            // Not adding `{ audio: true }` since we only want video now
+            navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+                video.src = window.URL.createObjectURL(stream);
+                video.play();
+            });
+        }
+    };
+    CapturePhotoComponent.prototype.stopVideoCamera = function () {
+        this.hideVedio = true;
+        var video1 = document.getElementById('video');
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            video1.src = window.URL.createObjectURL(stream);
+            for (var _i = 0, _a = stream.getTracks(); _i < _a.length; _i++) {
+                var tt = _a[_i];
+                tt.stop();
+            }
+        });
+    };
+    CapturePhotoComponent.prototype.onScan = function () {
+        // Elements for taking the snapshot
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        var video = document.getElementById('video');
+        context.drawImage(video, 0, 0, 640, 480);
+        this.stopVideoCamera();
+    };
+    CapturePhotoComponent.prototype.onHidePhoto = function () {
+        this.hidePhoto = true;
+    };
+    return CapturePhotoComponent;
+}());
+CapturePhotoComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_3" /* Component */])({
+        selector: 'app-capture-photo',
+        template: __webpack_require__(138),
+        styles: [__webpack_require__(135)]
+    }),
+    __metadata("design:paramtypes", [])
+], CapturePhotoComponent);
+
+//# sourceMappingURL=capture-photo.component.js.map
+
+/***/ }),
+
+/***/ 80:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ })
+
+},[163]);
+//# sourceMappingURL=main.bundle.js.map
--- src/app/capture-photo/capture-photo.component.ts
+++ src/app/capture-photo/capture-photo.component.ts
@@ -7,27 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CapturePhotoComponent implements OnInit {
 
-  constructor() { }
+  hideVedio : boolean;
+  hidePhoto : boolean;
+  constructor() {
+    this.hideVedio = true;
+    this.hidePhoto = true;
+   }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  startVideoCamera() {
+    this.hideVedio = false;
+    this.hidePhoto = true;
     // Grab elements, create settings, etc.
     var video = document.getElementById('video') as HTMLVideoElement;
-
     // Get access to the camera!
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       // Not adding `{ audio: true }` since we only want video now
       navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
         video.src = window.URL.createObjectURL(stream);
         video.play();
+        
       });
     }
   }
 
+  stopVideoCamera() {
+    this.hideVedio = true;
+    var video1 = document.getElementById('video') as HTMLVideoElement;
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+      video1.src = window.URL.createObjectURL(stream);
+      for (let tt of stream.getTracks())
+        tt.stop();
+    });
+  }
+
   onScan() {
     // Elements for taking the snapshot
-    var canvas : any = document.getElementById('canvas');
+    var canvas: any = document.getElementById('canvas');
     var context = canvas.getContext('2d');
-    var video = document.getElementById('video'); 
+    var video = document.getElementById('video');
     context.drawImage(video, 0, 0, 640, 480);
+    this.stopVideoCamera();
+  }
+  onHidePhoto(){
+    this.hidePhoto = true;
   }
 }
